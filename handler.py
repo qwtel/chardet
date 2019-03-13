b93ffe7ac4511db6455c7e44eedd449d82d0606d
Florian Klampfer <mail@qwtel.com>
2019-03-13 10:42:41 +0700

    Add CORS

--- handler.py
+++ handler.py
@@ -2,14 +2,31 @@ import json
 import base64
 import chardet
 
+cors_headers = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Credentials": True,
+}
+
 def detect(event, context):
-    rawdata = base64.b64decode(event['body'])
-    det = chardet.detect(rawdata)
-    return {
-        "statusCode": 200,
-        "headers": {
-          "Access-Control-Allow-Origin": "*",
-          "Access-Control-Allow-Credentials": True,
-        },
-        "body": json.dumps(det)
-    }
+    if not 'body' in event:
+        return {
+            "statusCode": 400,
+            "headers": cors_headers,
+            "body": json.dumps({ "error": "No request body" })
+        }
+
+    try:
+        rawdata = base64.b64decode(event['body'])
+        det = chardet.detect(rawdata)
+        return {
+            "statusCode": 200,
+            "headers": cors_headers,
+            "body": json.dumps(det)
+        }
+    except:
+        e = sys.exc_info()[0]
+        return {
+            "statusCode": 500,
+            "headers": cors_headers,
+            "body": json.dumps({ "error": "%s" % e })
+        }
